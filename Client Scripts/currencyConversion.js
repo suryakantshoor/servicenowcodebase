1251729db30ff40bdef4425acd1eb11fd9e591df
suryakantshoor <suryakantshoor@gmail.com>
2023-03-26 21:03:19 -0500

    Create currencyConversion.js
    This client script is used to update the price of the variable in the catalog item by using a [script include-currencyUtils](/servicenowpdi/script includes/currencyUtils.js)

---
+++ Client Scripts/currencyConversion.js
@@ -0,0 +1,17 @@
+function onSubmit() {
+    //Type appropriate comment here, and begin script below
+    var price = g_form.getValue('price');
+    var currency = g_form.getValue('currency');
+    var ga = new GlideAjax('CurrencyUtils'); //Scriptinclude/
+    ga.addParam('sysparm_name', 'convertValue'); //Method
+    ga.addParam('price', price); //Parameters
+    ga.addParam('currency', currency);
+    ga.getXMLAnswer(getResponse);
+
+    function getResponse(response) {
+        var res = JSON.parse(response);
+        g_form.setValue('price', res);
+        g_form.addInfoMessage(res);
+    }
+
+}
